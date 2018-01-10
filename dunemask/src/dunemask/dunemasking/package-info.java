41fabb17c0385ddfcfc13687ca8ae22da12475d7
Elijah Parker <34222254+Dunemask@users.noreply.github.com>
2018-01-10 12:13:14 -0700

    Finished Setting Up DM and basic stuff
    Complete

---
+++ dunemask/src/dunemask/dunemasking/package-info.java
@@ -0,0 +1,10 @@
+/**Tools for either working on, or setting up a Dunemasking Project
+ * 
+ * <p>Changelog Class: {@link dunemask.dunemasking.Changelog}</p>
+ * <p>Setup Class: {@link dunemask.dunemasking.Setup}</p>
+ * <p>Src Class: {@link dunemask.dunemasking.Src}</p>
+ * <p>Version Class: {@link dunemask.dunemasking.Version}</p>
+ *  @author Elijah
+ */
+package dunemask.dunemasking;
+
