4f0fe3e47aabf6de80a7a7450daac859e0787240
Dunemask <dunemask@gmail.com>
2018-01-10 16:06:01 -0700

    Basic Project Placed
    Contains Trace ammounts of epicness (Again using eclipse now)

---
+++ default project/src/lookhere/JASON.java
@@ -0,0 +1,27 @@
+/** 
+ * Dunemask Created This File on the Main Repository
+ * @contact Dunemask at dunemask@gmail.com
+ * commit = false
+ * File: lookhere.JASON.java
+ * Version: 0.1
+ * info: (Information About The Class)
+ * (To Change This Go To Window > Preferences 
+ * > Java > Code Style > Code Templates)
+ */
+package lookhere;
+
+/**
+ * @author Elijah
+ *
+ */
+public class JASON {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("HELLO WORLD! - Elijah");
+		//In Yo Face!
+	}
+
+}
