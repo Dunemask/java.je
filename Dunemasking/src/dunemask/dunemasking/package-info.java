f8f910186afad4106f151e9729ec3f8f46ad61e4
Dunemask <dunemask@gmail.com>
2018-01-11 20:54:01 -0700

    Dis

--- Dunemasking/src/dunemask/dunemasking/package-info.java
+++ Dunemasking/src/dunemask/dunemasking/package-info.java
@@ -4,6 +4,7 @@
  * <p>Setup Class: {@link dunemask.dunemasking.Setup}</p>
  * <p>Src Class: {@link dunemask.dunemasking.Src}</p>
  * <p>Version Class: {@link dunemask.dunemasking.Version}</p>
+ * <p>GitHub Station: {@link dunemask.dunemasking.GitHubStation}}</p>
  *  @author Elijah
  */
 package dunemask.dunemasking;
---
+++ Dunemasking/src/dunemask/resources/github/package-info.java
@@ -0,0 +1,15 @@
+/** 
+ * Dunemask Created This File on the Main Repository
+ * @contact Dunemask at dunemask@gmail.com
+ * commit = false
+ * File: dunemask.resources.github.package-info.java
+ * Version: 0.1
+ * info: (Information About The Class)
+ * (To Change This Go To Window > Preferences 
+ * > Java > Code Style > Code Templates)
+ */
+/**
+ * @author Elijah
+ *
+ */
+package dunemask.resources.github;
