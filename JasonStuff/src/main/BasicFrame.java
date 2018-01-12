1bf1a0c4ec67c73fc716633cb741f25f20c27874
JasonRoberts11 <33434307+JasonRoberts11@users.noreply.github.com>
2018-01-12 08:57:15 -0700

    D=Push

--- JasonStuff/src/main/BasicFrame.java
+++ JasonStuff/src/main/BasicFrame.java
@@ -1,25 +1,51 @@
 package main;
 
+import java.awt.Color;
 import java.awt.GraphicsConfiguration;
 import java.awt.HeadlessException;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class BasicFrame extends JFrame {
-
-	public BasicFrame() throws HeadlessException {
-		this.setTitle("Basic Frame");
-		this.setSize(800,800);
-		this.setResizable(true);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+public class BasicFrame extends JFrame{
+	Popup pop;
+	int xpos;
+	int ypos;
+	int num = 0;
+	JPanel jil;
+	BasicFrame dra;
+	boolean dead = true;
+	public BasicFrame(int x,int y,Popup z) throws HeadlessException {
+		xpos = x;
+		ypos = y;
+		pop = z;
+		this.setTitle("YOU WON");
+		this.setSize(300,300);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		this.setVisible(true);
 		this.setLocationRelativeTo(null);
-		JPanel jil = new JPanel();
-		jil.setLayout(null);
+		this.setLocation(x, y);
+		jil = new JPanel();
+		jil.setLayout(null);		
 		this.add(jil);
 		repaint();
 		revalidate();
-	}
 
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				dead = false;
+				dra = pop.Createnewframe(xpos+30, ypos-30);
+			}
+		});
+	}
+	public void Increm() {
+		num+=1;
+		jil.setBackground(new Color(num % 256, 5*num % 256, 100));
+		
+		//dra.Increm();
+	}
 }
--- JasonStuff/src/main/Popup.java
+++ JasonStuff/src/main/Popup.java
@@ -2,11 +2,32 @@
  * 
  */
 package main;
+
+import java.awt.Color;
+import java.awt.Frame;
+
+import javax.swing.JFrame;
+
 public class Popup {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("This is some cool dudes");
+		//BasicFrame f = new BasicFrame(0,0);
+		Popup t = new Popup();
+		BasicFrame fram = t.Createnewframe(500,500);
+		while(true) {
+			try {
+				Thread.sleep(100);
+				fram.Increm();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	public BasicFrame Createnewframe(int x,int y) {
+		BasicFrame g =  new BasicFrame(x,y,this);
+		return g;
 	}
 
 }
