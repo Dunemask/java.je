eec7f0f0cf55f9d3ac7e043090cd89ea3c123e04
Dunemask <dunemask@gmail.com>
2018-01-16 11:54:08 -0700

    Changing github

---
+++ Dunemasking/src/dunemask/dunemasking/GitHub.java
@@ -0,0 +1,32 @@
+/** 
+ * Dunemask Created This File on the Main Repository
+ * @contact Dunemask at dunemask@gmail.com
+ * commit = true
+ * File: dunemask.dunemasking.GitHub.java
+ * Version: 0.1
+ * info: (Information About The Class)
+ * (To Change This Go To Window > Preferences 
+ * > Java > Code Style > Code Templates)
+ */
+package dunemask.dunemasking;
+
+/**
+ * @author karib
+ *
+ */
+public class GitHub {
+	public static String repPath;
+	public static String lastAddedFilePath;
+	/**Version*/
+    final static double version = 4.1;
+    
+    /** Open GithubStation
+     **/
+    public static void Station(){
+    	Capture.startConsole();
+    	
+    	
+    }
+	
+
+}
