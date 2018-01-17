9e62c35be3ec72ca9af4e29aeca62ef4a43ed767
JasonRoberts11 <33434307+JasonRoberts11@users.noreply.github.com>
2018-01-17 07:48:19 -0700

    Hopefully this doesn't blow up

--- JasonStuff/src/main/BasicFrame.java
+++ JasonStuff/src/main/BasicFrame.java
@@ -3,49 +3,57 @@ package main;
 import java.awt.Color;
 import java.awt.GraphicsConfiguration;
 import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-
-public class BasicFrame extends JFrame{
-	Popup pop;
-	int xpos;
-	int ypos;
-	int num = 0;
+import javax.swing.Timer;
+import java.awt.color.*;
+import java.awt.Font;
+public class BasicFrame extends JFrame {
+	int tx;
+	int ty;
+	int num=0;
+	Popup ppup;
 	JPanel jil;
-	BasicFrame dra;
-	boolean dead = true;
-	public BasicFrame(int x,int y,Popup z) throws HeadlessException {
-		xpos = x;
-		ypos = y;
-		pop = z;
-		this.setTitle("YOU WON");
-		this.setSize(300,300);
+	public BasicFrame(int x, int y, Popup p) throws HeadlessException {
+		ppup = p;
+		tx = x;
+		ty = y;
+		this.setTitle("Basic Frame");
+		this.setSize(500,500);
 		this.setResizable(false);
 		this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		this.setVisible(true);
 		this.setLocationRelativeTo(null);
-		this.setLocation(x, y);
+		this.setLocation(tx, ty);
 		jil = new JPanel();
-		jil.setLayout(null);		
-		this.add(jil);
+		jil.setLayout(null);
+		JLabel k = new JLabel("!!!YOU WON A FREE CAR!!!");
+		k.setFont(new Font("Tahoma", Font.PLAIN, 40));
+		k.setBounds(0, 0, 500, 500);
+		k.setHorizontalAlignment(0);
+		jil.add(k);
+		getContentPane().add(jil);
 		repaint();
 		revalidate();
-
 		this.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
-				dead = false;
-				dra = pop.Createnewframe(xpos+30, ypos-30);
+				ppup.CreateBF(tx+20, ty-20);
 			}
 		});
-	}
-	public void Increm() {
-		num+=1;
-		jil.setBackground(new Color(num % 256, 5*num % 256, 100));
-		
-		//dra.Increm();
+		Timer timer = new Timer(10,new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				num+=10;
+				jil.setBackground(new Color(255-(num%256),num %256,1));
+			}
+		});
+		timer.start();
 	}
 }
--- JasonStuff/src/main/Popup.java
+++ JasonStuff/src/main/Popup.java
@@ -11,23 +11,13 @@ import javax.swing.JFrame;
 public class Popup {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("This is some cool dudes");
-		//BasicFrame f = new BasicFrame(0,0);
-		Popup t = new Popup();
-		BasicFrame fram = t.Createnewframe(500,500);
-		while(true) {
-			try {
-				Thread.sleep(100);
-				fram.Increm();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
+		Popup p = new Popup();
 	}
-	public BasicFrame Createnewframe(int x,int y) {
-		BasicFrame g =  new BasicFrame(x,y,this);
+	public Popup() {
+		CreateBF(600,600);
+	}
+	public BasicFrame CreateBF(int x,int y) {
+		BasicFrame g = new BasicFrame(x,y,this);
 		return g;
 	}
-
 }
