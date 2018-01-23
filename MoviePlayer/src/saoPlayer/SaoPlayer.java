67e696465a9d41741d7d23bde372234f4377b9fe
Dunemask <dunemask@gmail.com>
2018-01-22 21:44:51 -0700

    And again moved things around x

--- MoviePlayer/src/saoPlayer/SaoPlayer.java
+++ MoviePlayer/src/saoPlayer/SaoPlayer.java
@@ -70,7 +70,7 @@ public class SaoPlayer {
 			
 		});
 		getFiles.start();
-		String title = MovieLauncher.frame.getTitle();
+	
 		
 		
 		try {
@@ -80,6 +80,7 @@ public class SaoPlayer {
 			
 			e.printStackTrace();
 		}
+		String title = MovieLauncher.frame.getTitle();
 		MovieLauncher.current.getRepeatButton().setDisable(true);
 		
 		MovieLauncher.frame.setTitle(MovieLauncher.frame.getTitle()+" - "+files.get(playing).getName());
