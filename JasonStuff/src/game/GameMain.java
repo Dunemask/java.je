04b496d91202a9d10587383ff38d3fc165ce3592
JasonRoberts11 <33434307+JasonRoberts11@users.noreply.github.com>
2018-01-23 08:55:07 -0700

    IwillTrythis

--- JasonStuff/src/game/GameMain.java
+++ JasonStuff/src/game/GameMain.java
@@ -2,24 +2,58 @@ package game;
 
 import java.awt.Color;
 import java.awt.MouseInfo;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.event.MouseInputAdapter;
+import java.io.File;
+import java.awt.event.*;
 
 import dunemask.util.FileUtil;
 
 public class GameMain {
 	//Use dunemask.fileutil.getresource it will ignore the bin and will also work in jar form
 	//static ImageIcon image1 = new ImageIcon("bin/game/imgs/blok1.png");
-	
-	static ImageIcon image1 = new ImageIcon(FileUtil.getResource("game/imgs/blok1.png").getPath());
+	static int lengthf = new File("bin/game/imgs").listFiles().length;
+	static ImageIcon images[] = new ImageIcon[lengthf];
+	//static ImageIcon image1 = new ImageIcon(FileUtil.getResource("game/imgs/blok1.png").getPath());
 	public static void main(String[] args) {
+		for(int i = 0 ; i<lengthf;i++) {
+			images[i] = new ImageIcon("bin/game/imgs/blok"+(i+1)+".png");
+		}
 		JFrame f = new JFrame();
+		JLabel num = new JLabel("STUFF");
+		num.setSize(100,100);
+		num.setLocation(10,10);
+		int choose=0;
+		JPanel tab1 = new JPanel();
+		tab1.setLayout(null);
+		tab1.setBackground(new Color(200,200,200));
+		tab1.setSize(1024,256);
+		tab1.setLocation(0,768);
+		num.setText("NUM" + lengthf);
+		JButton[] labs = new JButton[lengthf];
+		for (int i=0; i<lengthf;i++) {
+			labs[i] = new JButton();
+			//labs[i].setText("blo "+ i);
+			labs[i].setIcon(images[i]);
+			labs[i].setSize(50,50);
+			labs[i].setLocation(i*50, 10);
+			tab1.add(labs[i]);
+			ActionListener al = new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					choose=1;
+				}
+			};
+			labs[i].addActionListener(al);
+			
+			
+		}
+		
+		
 		f.setTitle("GAME MAIN");
 		f.setSize(1024,1024);
 		f.setResizable(true);
@@ -27,21 +61,23 @@ public class GameMain {
 		f.setVisible(true);
 		f.setLocationRelativeTo(null);
 		f.setLayout(null);
+		f.add(tab1);
 		JPanel p = new JPanel();
 		p.setLayout(null);
 		p.setBackground(new Color(20,240,250));
 		p.setSize(512,512);
 		p.setLocation(256, 256);
 		f.add(p);
+		f.add(num);
 		f.repaint();
 		JPanel[][] AllSquares = new JPanel[32][32];
 		for(int i=0;i<10;i++) {
 			for (int j=0;j<10;j++) {
-				AllSquares[i][j] = Addsquare(i,j,p);
+				AllSquares[i][j] = Addsquare(i,j,p,choose);
 			}
 		}
 		f.repaint();
-		JLabel sel = new JLabel(image1);
+		JLabel sel = new JLabel(images[choose]);
 		sel.setSize(16,16);
 		p.add(sel);
 		p.addMouseListener(new MouseListener() {
@@ -50,27 +86,33 @@ public class GameMain {
 	        public void mouseEntered(MouseEvent me) { }
 	        public void mouseExited(MouseEvent me) { }
 	        public void mouseClicked(MouseEvent me) { 
-	        	AllSquares[me.getX()/16][me.getY()/16] = Addsquare(me.getX()/16,me.getY()/16,p);
+	        	//AllSquares[me.getX()/16][me.getY()/16] = Addsquare(me.getX()/16,me.getY()/16,p);
+	        	//System.out.println(me.getX());
 	        }
 	    });
 		p.addMouseMotionListener(new MouseInputAdapter() {
 			public void mouseMoved(MouseEvent me){
 				//System.out.println(me.getX());
 				sel.setLocation((me.getX()/16)*16, (me.getY()/16)*16);
-				
+			}
+			public void mouseDragged(MouseEvent me) {
+				AllSquares[me.getX()/16][me.getY()/16] = Addsquare(me.getX()/16,me.getY()/16,p,choose);
+				System.out.println(me.getX());
+				f.repaint();
 			}
 		});
 		
 		
 		
 	}
-	public static JPanel Addsquare(int x, int y,JPanel j){
+	public static JPanel Addsquare(int x, int y,JPanel j,int choose){
 		JPanel s  = new JPanel();
 		s.setSize(16, 16);
 		s.setLocation(x*16, y*16);
 		s.setBackground(new Color(10,10,100));
 		s.setLayout(null);
-		JLabel lab =  new JLabel(image1);
+		JLabel lab=(null);
+		lab =  new JLabel(images[choose]);
 		lab.setSize(16,16);
 		lab.setLocation(00, 0);
 		s.add(lab);
