5993d71776fbbf0c5bcc1efcbb6a01880b471f07
Dunemask <dunemask@gmail.com>
2018-01-26 10:51:53 -0700

    FXUTIL!

---
+++ default project/src/jfxTest/Call.java
@@ -0,0 +1,30 @@
+/** 
+ * Dunemask Created This File on the Main Repository
+ * @contact Dunemask at dunemask@gmail.com
+ * commit = false
+ * File: jfxTest.Call.java
+ * Version: 0.1
+ * info: (Information About The Class)
+ * (To Change This Go To Window > Preferences 
+ * > Java > Code Style > Code Templates)
+ */
+package jfxTest;
+
+import javafx.application.Platform;
+
+/**
+ * @author karib
+ *
+ */
+public class Call {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		RunApp.main(null);
+		RunApp.main(null);
+
+	}
+
+}
---
+++ default project/src/jfxTest/RunApp.java
@@ -0,0 +1,39 @@
+/** 
+ * Dunemask Created This File on the Main Repository
+ * @contact Dunemask at dunemask@gmail.com
+ * commit = false
+ * File: jfxTest.RunApp.java
+ * Version: 0.1
+ * info: (Information About The Class)
+ * (To Change This Go To Window > Preferences 
+ * > Java > Code Style > Code Templates)
+ */
+package jfxTest;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
+
+import javax.swing.SwingUtilities;
+import javafx.embed.swing.JFXPanel;
+import javafx.application.Platform;
+import javafx.stage.FileChooser;
+
+/**
+ * @author karib
+ *
+ */
+public class RunApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+		
+
+
+	}
+
+}
