86d5f91472d1a93854cf21c06ada0a134a26d84b
Elijah Parker <34222254+Dunemask@users.noreply.github.com>
2018-03-15 07:37:03 -0600

    CookiePIE

---
+++ School/src/ChessKnight.java
@@ -0,0 +1,51 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+
+/**
+ * 
+ */
+
+/**
+ * @author Karib
+ *
+ */
+public class ChessKnight {
+	static Scanner in = new Scanner(System.in);
+	static int row=1;
+	/**
+	 * @param args
+	 */
+	static int col=2;
+	public static void main(String[] args) {
+		JFrame f = new JFrame();
+		f.addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyPressed(KeyEvent evt) {
+	
+				
+			}
+
+			@Override
+			public void keyReleased(KeyEvent evt) {
+		
+				
+			}
+
+			@Override
+			public void keyTyped(KeyEvent evt) {
+		
+				
+			}
+			
+			
+			
+		});
+		
+		
+	}
+
+}
