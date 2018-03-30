9d9b34615d8396f6cd3bb93c1be218e7681fdc0c
Dunemask <kariboh@icloud.com>
2018-03-30 12:29:57 -0700

    Minor Github Changes

--- Dunemasking/src/dunemask/dunemasking/GitHub.java
+++ Dunemasking/src/dunemask/dunemasking/GitHub.java
@@ -63,7 +63,7 @@ public class GitHub {
     	Capture.closeConsole();
     	}
     
-	/** Get file From Repository
+	/** Get file From Dunemask Repository
 	 * @param genericPath Relative path to file from github tmp repository
 	 * @param reps Repository
 	 * @return file From dunemask.github.tmp directory by the specified path
@@ -72,6 +72,59 @@ public class GitHub {
 		String url = "https://github.com/Dunemask/"+reps+"/raw/master/"+genericPath;
 		
 		
+		File webFile = null;
+		try {
+		webFile = FileUtil.getWebFile(url);
+		}catch(Exception e) {
+			System.err.println("Resource not found at: "+url);		
+		}	
+		File file = webFile;
+		return file;
+		
+	}
+	
+	/** Get file From Dunemask Repository
+	 * @param genericPath Relative path to file from github tmp repository
+	 * @param reps Repository
+	 * @param out OutFile
+	 * @return file From dunemask.github.tmp directory by the specified path
+	 */
+	public static File gitFile(String reps,String genericPath,File out) {
+		String url = "https://github.com/Dunemask/"+reps+"/raw/master/"+genericPath;
+		try {
+		out = FileUtil.getWebFile(url,out);
+		}catch(Exception e) {
+			System.err.println("Resource not found at: "+url);		
+		}	
+		return out;
+		
+	}
+	
+	/** Get file From Dunemask Repository
+	 * @param genericPath Relative path to file from github tmp repository
+	 * @param reps Repository
+	 * @param user Github User
+	 * @return file From dunemask.github.tmp directory by the specified path
+	 */
+	public static File gitFileFromUser(String reps,String genericPath,String user,File out) {
+		String url = "https://github.com/"+user+"/"+reps+"/raw/master/"+genericPath;
+		try {
+		out = FileUtil.getWebFile(url,out);
+		}catch(Exception e) {
+			System.err.println("Resource not found at: "+url);		
+		}	
+		return out;
+		
+	}
+	
+	/** Get file From Dunemask Repository
+	 * @param genericPath Relative path to file from github tmp repository
+	 * @param reps Repository
+	 * @param user Github User
+	 * @return file From dunemask.github.tmp directory by the specified path
+	 */
+	public static File gitFileFromUser(String reps,String genericPath,String user) {
+		String url = "https://github.com/"+user+"/"+reps+"/raw/master/"+genericPath;
 		File webFile = null;
 		try {
 		webFile = FileUtil.getWebFile(url);
