ae3b4d81d985d4c9773958cb50a4f2867352c016
Elijah Parker <34222254+Dunemask@users.noreply.github.com>
2018-04-09 07:51:36 -0600

    Changed this

---
+++ Elijah's Other Projects/JTunes Test/src/Installer/Installer.java
@@ -0,0 +1,10 @@
+package Installer;
+
+public class Installer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Elijah's Other Projects/JTunes Test/src/jtunes/Jtunes.java
@@ -0,0 +1,10 @@
+package jtunes;
+
+public class Jtunes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
