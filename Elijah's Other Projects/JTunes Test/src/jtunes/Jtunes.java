d950c548fa67adcc4dcaf980e4ab2cdf84e8932d
Elijah Parker <34222254+Dunemask@users.noreply.github.com>
2018-04-09 08:00:11 -0600

    Changes

--- Elijah's Other Projects/JTunes Test/src/jtunes/Jtunes.java
+++ Elijah's Other Projects/JTunes Test/src/jtunes/Jtunes.java
@@ -1,10 +1,16 @@
 package jtunes;
 
-public class Jtunes {
+import java.io.File;
+
+import dunemask.util.xml.XMLMap;
+
+public class JTunes {
 	public static XMLMap library;
+	public static String JTunesPath = System.getProperty("user.home")+"/Music/JTunes/";
 	
 	public static void main(String[] args) {
-	
+		new File(JTunesPath).mkdirs();
+		
 
 	}
 
