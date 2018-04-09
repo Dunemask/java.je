99a1faab87019fe6a2487ffce39721344ba75abf
Elijah Parker <34222254+Dunemask@users.noreply.github.com>
2018-04-09 07:52:25 -0600

    Added Stuff

---
+++ Elijah's Other Projects/JTunes Test/src/frames/MainFrame.java
@@ -0,0 +1,14 @@
+package frames;
+
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+
+	public MainFrame() {
+	}
+
+
+}
