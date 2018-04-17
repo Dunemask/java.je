9b5ef5e0b688a57c771deb36e67c707d724a3827
Elijah Parker <Dunemask@users.noreply.github.com>
2018-04-17 16:41:54 -0600

    asdf

--- VoxelCraft/src/mc/MineCommands.java
+++ VoxelCraft/src/mc/MineCommands.java
@@ -3,6 +3,8 @@ package mc;
  * 
  */
 
+import minerender.VoxelCt;
+
 /**
  * @author Dunemask
  *
@@ -10,7 +12,26 @@ package mc;
 public class MineCommands {
 	
 	
-	public static void loadWorld() {
+	public static void HandleCommand(String fullCommand) {
+		
+		String[] com = fullCommand.split("\\s+");
+		String command = com[0];
+		if(!command.startsWith("/")) {
+			System.out.println(fullCommand);
+		}else {
+			command = command.substring(1, command.length());
+		}
+		
+		if(command.equalsIgnoreCase("gamemode")) {
+			if(com[1].equalsIgnoreCase("c")) {
+				Minecraft.vx.changeMode(VoxelCt.Creative);
+			}else if(com[1].equalsIgnoreCase("s")){
+				Minecraft.vx.changeMode(VoxelCt.Survival);
+			}else {
+				System.out.println("Invalid Argument");
+			}
+		}
+		
 		
 	}
 	
