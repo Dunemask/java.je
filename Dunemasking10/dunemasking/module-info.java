83045e4ccfc879ed632a73613397ad2ab8cc17c5
Elijah Parker <Dunemask@users.noreply.github.com>
2018-04-22 18:18:49 -0600

    moved things it's just really wyrd

---
+++ Dunemasking10/dunemasking/module-info.java
@@ -0,0 +1,21 @@
+/** Dunemasking is an opensource program that has some basic java tools
+ * 
+ * @author dunemask
+ * 
+ * */
+module dunemasking{
+	requires  java.base;
+	requires  transitive  java.desktop;
+	requires  transitive  javafx.graphics;
+	requires  transitive  javafx.media;
+	requires  transitive  javafx.fxml;
+	requires  transitive  javafx.base;
+	requires  transitive  javafx.swing;
+	requires  transitive  javafx.controls;
+	requires  transitive  java.scripting;
+	exports dunemask.util;
+	exports dunemask.objects;
+	exports dunemask.objects.movieplayer;
+	exports dunemask.util.xml;
+	exports dunemask.other;
+}
