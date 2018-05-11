9ee2ebb9e10edfa51364720d246bb5692f0276e0
Elijah Parker <34222254+Dunemask@users.noreply.github.com>
2018-05-11 16:44:13 -0600

    small changes completed the drm

--- ElijahStuff/summer2018/scapitals/src/scapitals/QuizGenny.java
+++ ElijahStuff/summer2018/scapitals/src/scapitals/QuizGenny.java
@@ -4,9 +4,14 @@
 package scapitals;
 
 import java.io.File;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 
-import dunemask.util.ResourceUtil;
-import dunemask.util.rw.RW;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Collections;
+
+import dunemask.util.xml.Runemap;
 
 /**
  * @author Dunemask
@@ -14,11 +19,25 @@ import dunemask.util.rw.RW;
  */
 public class QuizGenny {
 
-	public static void main(String[] args) {
-		File caps = ResourceUtil.writeUrl(QuizGenny.class.getResource("/scapitals/Caps.txt"));
+	public static void main(String[] args) throws URISyntaxException {
+		/*File caps = ResourceUtil.writeUrl(QuizGenny.class.getResource("/scapitals/Caps.txt"));
 		File states = ResourceUtil.writeUrl(QuizGenny.class.getResource("/scapitals/States.txt"));
 		String[] clines = RW.readAll(caps);
-		String[] slines = RW.readAll(states);
+		String[] slines = RW.readAll(states);*/
+		File drm = new File(QuizGenny.class.getResource("/scapitals/sandc.drm").getFile());
+		Runemap map = Runemap.parseRunemap(drm);
+		String dtop = System.getProperty("user.home")+"/Desktop/";
+		new File(dtop+"tests/").mkdirs();
+		Random r = new Random();
+		ArrayList<String> states = new ArrayList<String>(map.getFullMap().keySet());
+		ArrayList<String> caps = new ArrayList<String>(map.getFullMap().values());
+		for(int i=0;i<states.size();i++) {
+			System.out.println(states.get(i)+","+caps.get(i));
+		}
+
+		
+		
+		
 		
 		
 		
