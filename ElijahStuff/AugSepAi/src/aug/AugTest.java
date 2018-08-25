5a483631850fff6c1e7057dd1f804dfd27c378ca
Elijah Parker <34222254+Dunemask@users.noreply.github.com>
2018-08-24 18:12:41 -0600

    aitest

--- ElijahStuff/AugSepAi/src/aug/AugTest.java
+++ ElijahStuff/AugSepAi/src/aug/AugTest.java
@@ -16,20 +16,23 @@ public class AugTest {
 	
 	public static void main(String[] args) {
 		var rand = new Random();
-		int ss = 10;
+		int ss = 2;
 		int lc = 5;
 		var subs = new ArrayList<FoodBot>(ss);
 		for(int i=0;i<subs.size();i++) {
-			var fb = new FoodBot();
+			var fb = new FoodBot(3);
 			if(rand.nextBoolean()==true) {
 				fb.pos = 1;
 			}else {
 				fb.pos = -1;
 			}
+			fb.lastMove = fb.pos;
 			subs.add(fb);
 		}
 		
 		int foodDis = rand.nextInt(10);
+		System.out.println(subs.get(0).inodes.length);
+		
 		
 		
 		
@@ -42,6 +45,29 @@ public class AugTest {
 		
 		
 	}
+	
+	
+	public static int move(FoodBot cb) {
+		int move = 0;
+		/*int res1=0;
+		int res2=0;
+		if(warmer(res1,res2)) {
+			move = cb.lastMove;
+		}else {
+			
+		}*/
+		
+		
+		return move;
+	}
+	
+	/** Returns if Choice 1 is the prominent choice
+	 * 
+	 * */
+	public static boolean warmer(int res1,int res2) {
+		return res1>res2;
+	}
+	
 	
 	public static double connection() {
 		int con = 0;
--- ElijahStuff/AugSepAi/src/aug/FoodBot.java
+++ ElijahStuff/AugSepAi/src/aug/FoodBot.java
@@ -1,13 +1,26 @@
 package aug;
 
+import java.util.Random;
+
 public class FoodBot {
 
 	public int health;
 	public int pos;
+	public int[] inodes;
+	public int[] onodes;
 
-	public FoodBot() {
+	public FoodBot(int inputSize) {
+		Random r = new Random();
 		this.setHealth(10);
 		this.pos = 0;
+		this.inodes = new int[inputSize];
+		this.onodes = new int[inputSize-1];
+		for(int i=0;i<inodes.length;i++) {
+			inodes[i] = r.nextInt(10);
+		}
+		
+		
+		
 	}
 
 
