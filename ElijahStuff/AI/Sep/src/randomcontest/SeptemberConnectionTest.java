8013423fe5ea13fa832313867e98944b7f263be0
Elijah Parker <34222254+Dunemask@users.noreply.github.com>
2018-08-30 10:38:02 -0600

    Updates

--- ElijahStuff/AI/Sep/src/randomcontest/SeptemberConnectionTest.java
+++ ElijahStuff/AI/Sep/src/randomcontest/SeptemberConnectionTest.java
@@ -15,19 +15,44 @@ public class SeptemberConnectionTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		var in = new Inode[3];
+		var in = new Inode[5];
 		for(int i=0;i<in.length;i++) {
-			in[i] = new Inode(i,new Random().nextInt(50));
+			in[i] = new Inode(i,/*new Random().nextInt(0)*/1);
 		}
-		var nl = new NetLayer(in,3);
+		var nl = new NetLayer(in,2);
 		nl.runLayer();
 		var con = nl.getConnections();
-		for(int i=0;i<con.length;i++) {
+		/*for(int i=0;i<con.length;i++) {
 			System.out.println(con[i]);
+		}*/
+		var onodes = nl.getOnodes();
+		for(int i=0;i<onodes.length;i++) {
+			System.out.println(onodes[i]);
+		}
+		System.out.println("__________________");
+		System.out.println("NODE:"+getBest(onodes));
+		
+	}
+	
+	public static int getBest(Onode[] nodes) {
+		var index = 0;
+		
+		
+		
+		for(int i=0;i<nodes.length;i++) {
+			
+			
 		}
 		
-
+		
+		
+		
+		
+		return index;
+		
+		
 	}
+	
 
 	
 }
